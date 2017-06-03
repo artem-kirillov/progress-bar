be75aabfa77436ee995668c8e8ca462ebed8055f
artem.kirillov <sir.kirillov@gmail.com>
2017-06-03 15:05:56 +0300

    Added render rate limiter

--- src/main/scala/p1bar/ProgressBar.scala
+++ src/main/scala/p1bar/ProgressBar.scala
@@ -34,40 +34,57 @@ class BarFormatter(barFormat: BarFormat = AsciiBarFormat, unit: String = "it", u
   private val shortFmt = DateTimeFormatter.ofPattern("mm:ss")
 
   def format(n: Int, total: Int, elapsed: Duration): String = {
-    require(n <= total)
-    require(n >= 0 && total >= 0)
+    require(n <= total && total > 0)
+    require(n >= 0)
 
-    val leftBar = percentage(n, total)
-    val nBars = Math.max(1, ncols - leftBar.length - 1)
+    val leftBarStr = leftBar(n, total)
+    val rightBarStr = rightBar(n, total, elapsed)
 
-    val elapsedFmt = formatInterval(elapsed)
+    val nBars = Math.max(1, ncols - leftBarStr.length - rightBarStr.length - 2)
+    val bar = if (nBars > 6) " " + progressBar(n, total, nBars) + " " else "|"
 
-    val rate = 1.0 * n / elapsed.toSeconds
-    val rateFmt = f"$rate%5.2f$unit%s/s"
-
-    val remainingFmt = formatInterval(FiniteDuration(((total - n) / rate).toLong, TimeUnit.SECONDS))
-
-    val rightBar = s"$n/$total [$elapsedFmt<$remainingFmt, $rateFmt]"
-    s"$leftBar ${progressBar(n, total, nBars)} $rightBar"
+    s"$leftBarStr$bar$rightBarStr"
   }
+
+  def format(n: Int, elapsed: Duration): String = rightBar(n, elapsed)
 
   private def formatInterval(int: Duration): String = {
     val inst = Instant.ofEpochMilli(int.toMillis).atZone(ZoneId.systemDefault()).toLocalDateTime
     if (int.toHours >= 1) longFmt.format(inst) else shortFmt.format(inst)
   }
 
-  private def percentage(n: Int, total: Int): String = {
-    val v: Double = 100.0 * n / total
+  private def leftBar(n: Int, total: Int): String = {
+    val v: Double = 100d * n / total
     f"$v%.1f%%"
   }
 
   private def progressBar(n: Int, total: Int, nBars: Int): String = {
-    val totalLength = nBars - barFormat.leftBoundary.length - barFormat.rightBoundary.length
-    val frac = 1.0 * n / total
-    val done = (frac * totalLength).toInt
-    val remaining = totalLength - done
+    val bodyLength = nBars - barFormat.leftBoundary.length - barFormat.rightBoundary.length
+    val frac = n.toDouble / total
+    val done = (frac * bodyLength).toInt
+    val remaining = bodyLength - done
 
     s"${barFormat.leftBoundary}${barFormat.bar * done}${barFormat.empty * remaining}${barFormat.rightBoundary}"
+  }
+
+  private def rightBar(n: Int, total: Int, elapsed: Duration): String = {
+    val elapsedFmt = formatInterval(elapsed)
+
+    val rate = n.toDouble / elapsed.toSeconds
+    val rateFmt = f"$rate%5.2f$unit%s/s"
+
+    val remainingFmt = formatInterval(FiniteDuration(((total - n) / rate).toLong, TimeUnit.SECONDS))
+
+    s"$n/$total [$elapsedFmt<$remainingFmt, $rateFmt]"
+  }
+
+  private def rightBar(n: Int, elapsed: Duration): String = {
+    val elapsedFmt = formatInterval(elapsed)
+
+    val rate = n.toDouble / elapsed.toSeconds
+    val rateFmt = f"$rate%5.2f$unit%s/s"
+
+    s"$n [$elapsedFmt, $rateFmt]"
   }
 }
 
@@ -76,26 +93,39 @@ trait Updater {
 }
 
 class ProgressBar(total: Int, barFormatter: BarFormatter) {
+  def this(barFormatter: BarFormatter) = this(ProgressBar.UnknownTotal, barFormatter)
   def this() = this(ProgressBar.UnknownTotal, new BarFormatter())
 
   def this(total: Int) = this(total, new BarFormatter())
 
   private lazy val console = new PrintStream(System.err, true, "UTF-8")
+  private val renderInterval: Long = TimeUnit.MILLISECONDS.toNanos(100)
+
+  private var startTime: Long = _
   private var n = 0
   private var lastLen = 0
-  private var startTime: Long = _
+  private var lastRenderTime: Long = 0
 
   private def now(): Long = System.nanoTime
 
   private def update(incr: Int): Unit = {
-    val curTime  = now()
-
     require(incr >= 0)
     n += incr
 
-    val elapsed = FiniteDuration(curTime - startTime, TimeUnit.NANOSECONDS)
+    if (now() - lastRenderTime > renderInterval || n == total) {
+      render()
+      lastRenderTime = now()
+    }
+  }
 
-    val barLine: String = barFormatter.format(n, total, elapsed)
+  private def render(): Unit = {
+    val elapsed = FiniteDuration(now() - startTime, TimeUnit.NANOSECONDS)
+
+    val barLine: String = if (total == ProgressBar.UnknownTotal) {
+      barFormatter.format(n, elapsed)
+    } else {
+      barFormatter.format(n, total, elapsed)
+    }
     val padding: String = " " * Math.max(lastLen - barLine.length, 0)
 
     console.print(s"\r$barLine$padding")
@@ -121,16 +151,16 @@ class ProgressBar(total: Int, barFormatter: BarFormatter) {
 }
 
 object ProgressBar {
-  private val UnknownTotal: Int = -1
+  val UnknownTotal: Int = -1
 }
 
 object Main extends App {
-  val its = 60
+  val its = 6000
 
-  val progress = new ProgressBar(its, new BarFormatter(ncols = 30))
+  val progress = new ProgressBar(new BarFormatter(ncols = 5))
   progress meter { updater =>
     (1 to its).foreach { i =>
-      Thread.sleep(500)
+      Thread.sleep(1)
       updater.update(1)
     }
   }
